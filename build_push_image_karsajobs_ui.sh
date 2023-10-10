af88e1a96db02093dab4ecab65128bdcf26d2264
rachmandev <rachman@rachman.dev>
2023-10-10 22:20:42 +0700

    enable login

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,8 +1,8 @@
 echo "================ Melakukan Build Image ============" #menampilkan teks ke layar hasil dari perintah echo
 docker build -t karsajobs-ui:latest . #melakukan Build Image Dengan Dockerfile dengan nama image karsajobs dan memiliki tag latest
 echo "===== Melakukan Login Ke Github & Docker Hub ======" #menampilkan teks ke layar hasil dari perintah echo
-#docker login ghcr.io --username rachmandev --password $GITHUB_TOKEN #untuk login ke github package dengan username rachmandev dan password yang disimpan di variabel GITHUB_TOKEN jika menggunakan password-stdin tidak bisa login
-#docker login --username rachmandev --password $DOCKER_TOKEN #untuk login ke docker hub dengan username rachmandev dan password yang disimpan di dalam variabel DOCKER_TOKEN jika menggunakan --password-stdin tidak bisa login 
+docker login ghcr.io --username rachmandev --password $GITHUB_TOKEN #untuk login ke github package dengan username rachmandev dan password yang disimpan di variabel GITHUB_TOKEN jika menggunakan password-stdin tidak bisa login
+docker login --username rachmandev --password $DOCKER_TOKEN #untuk login ke docker hub dengan username rachmandev dan password yang disimpan di dalam variabel DOCKER_TOKEN jika menggunakan --password-stdin tidak bisa login 
 echo "Melakukan Push Ke Github Packages & Docker Hub" #menampilkan teks ke layar hasil dari perintah echo
 docker tag karsajobs-ui:latest ghcr.io/rachmandev/karsajobs-ui:latest #mengubah nama dari karsajobs:latest  menjadi ghcr.io/rachmandev/karsajobs:latest menggunakan tag latest
 docker tag karsajobs-ui:latest rachmandev/karsajobs-ui:latest #mengubah nama dari karsajobs:latest  menjadi rachmandev/karsajobs:latest menggunakan tag latest
